a761d7a5d337b556d0e9838b9872f0650264fa17
Kamal Verma <kamal.verma@myndsol.com>
2025-09-16 11:29:10 +0530

    SpaceAfterDot

---
+++ src/main/java/com/interview/SpaceAfterDot.java
@@ -0,0 +1,27 @@
+package com.interview;
+
+/**
+ * kamal.verma-16-Sep-2025-11:25:43 am
+ */
+public class SpaceAfterDot {
+
+	/**
+	* @description - 
+	* @param -
+	* @return -
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpaceAfterDot spaceAfterDot = new SpaceAfterDot();
+		spaceAfterDot.testSpace();
+		spaceAfterDot .testSpace();
+		spaceAfterDot. testSpace();
+
+	}
+	
+	private void testSpace() {
+		System.out.println("insult");
+	}
+
+}
